da3e8434c95b2a90dd2b62c550c150a6261c5f32
Дмитрий Кубарев <kubarevds@icloud.com>
2025-05-13 20:25:30 +0300

    add footer

--- src/shared/components/layout/Footer/Footer.tsx
+++ src/shared/components/layout/Footer/Footer.tsx
@@ -2,12 +2,82 @@ import { Container } from "@/shared/components/ui/containers/Container/Container
 import { TFooter } from "@/shared/typing/elements.type";
 import clsx from "clsx";
 import { FC } from "react";
+
+import { APP_PAGES } from "@/shared/dashboard/app.dashboard";
+import { LogoLink } from "../../parts/LogoLink/LogoLink";
+import { FooterLink } from "../../ui/FooterLink/FooterLink";
 import styles from "./Footer.module.css";
 
 export const Footer: FC<TFooter> = ({ className, ...rest }) => {
+  const skins = APP_PAGES.getSkins();
+  const balance = APP_PAGES.getBalance();
+  const useful = APP_PAGES.getUseful();
+
   return (
-    <footer className={clsx(styles.footer, className)} {...rest}>
-      <Container>footer</Container>
+    <footer className={clsx(styles.root, className)} {...rest}>
+      <Container>
+        <nav className={styles.nav}>
+          <div>
+            <LogoLink className={styles.logo} />
+            <p className={styles.text}>
+              Надежный сервис для покупки скинов  пополнения баланса
+            </p>
+          </div>
+          {skins && (
+            <div>
+              <p className={styles.title}>Маркет</p>
+              <ul>
+                {skins.map((item, index) => (
+                  <li key={index}>
+                    <FooterLink href={item.slug}>{item.name}</FooterLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {balance && (
+            <div>
+              <p className={styles.title}>Баланс</p>
+              <ul>
+                {balance.map((item, index) => (
+                  <li key={index}>
+                    <FooterLink href={item.slug}>{item.name}</FooterLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {useful && (
+            <div>
+              <p className={styles.title}>Полезное</p>
+              <ul>
+                {useful.map((item, index) => (
+                  <li key={index}>
+                    <FooterLink href={item.slug}>{item.name}</FooterLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </nav>
+
+        <div className={styles.bot}>
+          <div>
+            <p className={styles.copy}>© 2025 DonViche, Все права защищены</p>
+            <p className={styles.copy}>
+              Powered by Steam. Not affileated with Valve Corp.
+            </p>
+            <div className={styles.links}>
+              <FooterLink href={"/privacy"} target="_blank">
+                Политика конфиденциальности
+              </FooterLink>
+              <FooterLink href={"/terms"} target="_blank">
+                Публичная оферта
+              </FooterLink>
+            </div>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 };
--- src/shared/components/parts/LogoLink/LogoLink.tsx
+++ src/shared/components/parts/LogoLink/LogoLink.tsx
@@ -1,11 +1,13 @@
+import { TAnchor } from "@/shared/typing/elements.type";
+import clsx from "clsx";
 import Link from "next/link";
 import { FC } from "react";
 import { IconLogo } from "../../ui/svg/IconLogo";
 import styles from "./LogoLink.module.css";
 
-export const LogoLink: FC = () => {
+export const LogoLink: FC<TAnchor> = ({ className, ...rest }) => {
   return (
-    <Link href={"/"} className={styles.logo}>
+    <Link href={"/"} className={clsx(styles.logo, className)} {...rest}>
       <IconLogo />
     </Link>
   );
