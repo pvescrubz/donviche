2405c175341bdbdba53f74ba871afc5b25fd30df
Дмитрий Кубарев <kubarevds@icloud.com>
2025-05-14 00:44:48 +0300

    add hero bg / change HeroSecondary, change images

--- src/shared/components/sections/Hero/HeroBasic/HeroBasic.tsx
+++ src/shared/components/sections/Hero/HeroBasic/HeroBasic.tsx
@@ -8,39 +8,52 @@ export const HeroBasic: FC<TDiv> = () => {
   return (
     <>
       <div className={styles.root}>
-        <picture className={styles.image}>
+        <picture className={styles.fire}>
           <source
-            media="(max-width: 768px)"
-            className={styles.image}
-            srcSet="/images/hero_small.webp"
+            media="(max-width: 1024px)"
+            className={styles.fire}
+            srcSet="/images/hero-bg.webp"
             type="image/webp"
           />
           <source
-            media="(min-width: 769px) and (max-width: 1224px)"
-            srcSet="/images/hero_medium.webp"
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1225px)"
+            media="(min-width: 1025px)"
             className={styles.image}
-            srcSet="/images/hero_big.webp"
+            src="/images/1x1.webp"
             type="image/webp"
           />
           <img
-            src="/images/hero_big.webp"
+            src="/images/1x1.webp"
+            alt="Hero Background"
+            className={styles.fire}
+          />
+        </picture>
+        <picture className={styles.image}>
+          <source
+            media="(max-width: 1024px)"
+            className={styles.image}
+            srcSet="/images/hero-m.webp"
+            type="image/webp"
+          />
+          <source
+            media="(min-width: 1025px)"
+            className={styles.image}
+            srcSet="/images/hero-l.webp"
+            type="image/webp"
+          />
+          <img
+            src="/images/hero-l.webp"
             alt="Hero Background"
             className={styles.image}
           />
         </picture>
         <Container className={styles.wrapper}>
-          <h1 className={styles.title}>КУПИТЬ СКИНЫ CS2 & DOTA 2</h1>
+          <h1 className={styles.title}>КУПИТЬ СКИНЫ CS2 & DOTA 2</h1>
           <p className={styles.text}>
             Получи уникальные скины которые выделят тебя на поле боя! Широкий
             выбор редких предметов, доступные цены и мгновенная доставка.
           </p>
           <div className={styles.buttons_wrapper}>
             <ButtonLink
-              isPublicLink
               href="/skins"
               size="large"
               variant="primary"
@@ -49,7 +62,6 @@ export const HeroBasic: FC<TDiv> = () => {
               КУПИТЬ СКИНЫ
             </ButtonLink>
             <ButtonLink
-              isPublicLink
               href="/skins"
               size="large"
               variant="secondary"
--- src/shared/components/sections/Hero/HeroSecondary/HeroSecondary.tsx
+++ src/shared/components/sections/Hero/HeroSecondary/HeroSecondary.tsx
@@ -6,67 +6,67 @@ import styles from "./HeroSecondary.module.css";
 
 export const HeroSecondary: FC<TDiv> = ({}) => {
   return (
-    <Container className={styles.wrapper}>
-      <div className={styles.wrapper_item}>
+    <Container className={styles.root}>
+      <div className={styles.item}>
         <Image
-          src="/images/heroicon1.webp"
+          src="/images/heroicon1.png"
           alt="alt"
           quality={100}
           width={80}
           height={80}
-          className="image"
+          className={styles.image}
         />
         <div className={styles.text_wrapper}>
-          <h2 className={styles.title}>Широкий выбор скинов</h2>
+          <p className={styles.title}>Широкий выбор скинов</p>
           <p className={styles.text}>
             У нас представлены тысячи редких и эксклюзивных скинов для Dota 2
             и CS2.
           </p>
         </div>
       </div>
-      <div className={styles.wrapper_item}>
+      <div className={styles.item}>
         <Image
-          src="/images/heroicon2.webp"
+          src="/images/heroicon2.png"
           alt="alt"
           quality={100}
           width={80}
           height={80}
-          className="image"
+          className={styles.image}
         />
         <div className={styles.text_wrapper}>
-          <h2 className={styles.title}>Мгновенная доставка</h2>
+          <p className={styles.title}>Мгновенная доставка</p>
           <p className={styles.text}>
             После оплаты скины моментально доставляются на ваш аккаунт.
           </p>
         </div>
       </div>
-      <div className={styles.wrapper_item}>
+      <div className={styles.item}>
         <Image
-          src="/images/heroicon3.webp"
+          src="/images/heroicon3.png"
           alt="alt"
           quality={100}
           width={80}
           height={80}
-          className="image"
+          className={styles.image}
         />
         <div className={styles.text_wrapper}>
-          <h2 className={styles.title}>Доступные цены</h2>
+          <p className={styles.title}>Доступные цены</p>
           <p className={styles.text}>
             Мы предлагаем конкурентные цены и регулярные скидки.
           </p>
         </div>
       </div>
-      <div className={styles.wrapper_item}>
+      <div className={styles.item}>
         <Image
-          src="/images/heroicon4.webp"
+          src="/images/heroicon4.png"
           alt="alt"
           quality={100}
           width={80}
           height={80}
-          className="image"
+          className={styles.image}
         />
         <div className={styles.text_wrapper}>
-          <h2 className={styles.title}>Безопасные платежи</h2>
+          <p className={styles.title}>Безопасные платежи</p>
           <p className={styles.text}>
             Ваши данные защищены. Покупайте скины с уверенностью в полной
             безопасности.
