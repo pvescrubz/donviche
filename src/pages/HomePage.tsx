a11bc3b27c132288bcf6edc61a241e61b5b722c3
qseven <qseven2012@gmail.com>
2025-05-13 20:24:04 +0300

    add buylinks

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
+import { BuySkinsHome } from "@/shared/components/sections/BuySkins/BuySkins";
 import { Hero } from "@/shared/components/sections/Hero/Hero";
 
 export const HomePage = () => {
-  return <Hero />;
+  return  (
+    <>
+    <Hero />
+    <BuySkinsHome />
+    </>
+  
+ )
 };
---
+++ src/shared/components/sections/BuySkins/BuySkins.tsx
@@ -0,0 +1,31 @@
+import { TDiv } from "@/shared/typing/elements.type";
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+import { Container } from "../../ui/containers/Container/Container";
+import { Section } from "../../ui/containers/Section/Section";
+import { Title } from "../../ui/Title/Title";
+import styles from "./BuySkins.module.css";
+export const BuySkinsHome: FC<TDiv> = () => {
+  return  (
+  <Section>
+    <Container>
+      <Title>
+    КУПИТЬ СКИНЫ
+      </Title>
+      <div className={styles.wrapper}>
+        <Link href="href" className={styles.link}>
+        <Image src="/images/CS.webp" alt="alt" fill  className={styles.img} />
+        </Link>
+        <Link href="href" className={styles.link}>
+        <Image src="/images/Dota.webp" alt="alt" fill className={styles.img} />
+        </Link>
+        <Link href="href" className={styles.link}>
+        <Image src="/images/Rust.webp" alt="alt" fill className={styles.img} />
+        </Link>
+      </div>
+    </Container>  
+    
+    </Section>
+  )
+};
